57871c8ca7644278e61f9aad4c2cfb8d554f2a59
Lydia Stiffler <llstiffler@uga.edu>
2017-09-29 15:48:28 -0400

    annotated lm script

--- keys/lm/lm-key.R
+++ keys/lm/lm-key.R
@@ -1,10 +1,14 @@
+# Generalized key to evaluating the likelihood equations and joint posterior
+# probabilites for a simple linear model with 2 betas
 
+# Generate some random data
 n <- 100
 x <- rnorm(n) # Covariate
 beta0 <- -1
 beta1 <- 1
 sigma <- 2
 
+# Evaluate the mean and responses from the distributions
 mu <- beta0 + beta1*x
 y <- rnorm(n, mu, sigma)
 
@@ -15,8 +19,9 @@ summary(lm1 <- lm(y~x))
 
 
 
-## Likelihood
+## Evaluate the likelihood of the model
 
+# Create a negative log-likelihood function
 nll <- function(pars) {
     beta0 <- pars[1]
     beta1 <- pars[2]
@@ -26,6 +31,7 @@ nll <- function(pars) {
     -sum(ll)
 }
 
+# Variable inputs for nll function 
 starts <- c(beta0=0,beta1=0,sigma=1)
 
 nll(starts)
@@ -38,15 +44,20 @@ nll(starts2)
 
 
 
-## Minimize neg log-like
+## Minimize negative log-likelihood
 
+# The optim function minimizes the neg log-like using the set of inital values to be optimized over.
+# Hessian = TRUE returns the Hessian matrix (2nd order partial derivatives)
 fm <- optim(starts, nll, hessian=TRUE)
 fm
 
+# $par returns the best set of parameters found for the maximum likelihood estimator (mles)
 mles <- fm$par
 
+# Obtain the variance-covariance matrix by solving the Hessian matrix
 vcov <- solve(fm$hessian)
 
+# Standard errors taken from the square-root of the diagonals on the variance-covariance matrix 
 SEs <- sqrt(diag(vcov))
 
 cbind(Est=mles, SE=SEs)
@@ -61,34 +72,56 @@ cbind(Est=mles, SE=SEs)
 ## Gibbs sampler
 
 
-
+# Create a function "lm.gibbs" that uses input data to run n iterations using 
+# starting values and tuning values 
 lm.gibbs <- function(data, niter=10000,
                      start, tune) {
 
+# Create a matrix to store values from iterations   
 samples <- matrix(NA, niter, 3)
 colnames(samples) <- c("beta0", "beta1", "sigma")
 
+# Store starting values 
 beta0 <- start[1]
 beta1 <- start[2]
 sigma <- start[3]
 
 for(iter in 1:niter) {
+    ### Propose candidates values for each probabilites 
+  
     ## Sample from p(beta0|dot)
     mu <- beta0 + beta1*x
+    # Obtain the neg log-likelihood of the reponse variable distribution 
+    # Assume a normal distribution 
     ll.y <- sum(dnorm(y, mu, sigma, log=TRUE))
+    # Prior probability of beta0
     prior.beta0 <- dnorm(beta0, 0, 1000, log=TRUE)
-
+    
+    # Propose a candidate value for each beta0 based around the known value of beta0
+    # Tuning value allows for exploration of values around beta0 by oscillating values above and below beta0
+    # Tuning is a trial and error process and comes into play when assessing rejection rates 
     beta0.cand <- rnorm(1, beta0, tune[1])
+    # Update mu for the candidate beta0
     mu.cand <- beta0.cand + beta1*x
+    # Re-evaluate the summation of log-like of the reponse distribution use the candidate mu 
     ll.y.cand <- sum(dnorm(y, mu.cand, sigma, log=TRUE))
+    # Obtain a new prior probabilty for the candidate beta0
     prior.beta0.cand <- dnorm(beta0.cand, 0, 1000, log=TRUE)
 
+    # Metropolis-Hastings Ratio is a Markov chain Monte Carlo (MCMC) method used to 
+    # obtain a sequence of random sample from a probabilty distribution 
+    
+    # P(data | candidate beta0) * P(candidate beta0) / P(data | beta0) * P(beta0)  
     mhr <- exp((ll.y.cand+prior.beta0.cand) - (ll.y+prior.beta0))
+    
+    # If the MH ratio is greater than random deviates of uniform distribution than
+    # update beta0 with the new candidate beta0
     if(runif(1) < mhr) {
         beta0 <- beta0.cand
     }
 
     ## Sample from p(beta1|dot)
+    # Similar code instructions as used for beta0
     mu <- beta0 + beta1*x
     ll.y <- sum(dnorm(y, mu, sigma, log=TRUE))
     prior.beta1 <- dnorm(beta1, 0, 1000, log=TRUE)
@@ -105,29 +138,39 @@ for(iter in 1:niter) {
 
     ## Sample from p(sigma|dot)
     ll.y <- sum(dnorm(y, mu, sigma, log=TRUE))
+    # Assume a uniform distribution  
     prior.sigma <- dunif(sigma, 0, 1000, log=TRUE)
 
-    ## ## Symmetric proposal distribution
-    ## sigma.cand <- rnorm(1, sigma, tune[3])
-    ## if(sigma.cand>0) {
-    ##     ll.y <- sum(dnorm(y, mu, sigma, log=TRUE))
-    ##     prior.sigma <- dunif(sigma, 0, 1000, log=TRUE)
+    # ## Symmetric proposal distribution
+    # # Should this be runif? 
+    #  sigma.cand <- rnorm(1, sigma, tune[3])
+    #  # Sigma must be postive 
+    #  if(sigma.cand>0) {
+    #      ll.y <- sum(dnorm(y, mu, sigma, log=TRUE))
+    #      prior.sigma <- dunif(sigma, 0, 1000, log=TRUE)
+    # 
+    #      ll.y.cand <- sum(dnorm(y, mu, sigma.cand, log=TRUE))
+    #      prior.sigma.cand <- dunif(sigma.cand, 0, 1000, log=TRUE)
+    #  }
+    #     mhr <- exp((ll.y.cand+prior.sigma.cand) - (ll.y+prior.sigma))
 
-    ##     ll.y.cand <- sum(dnorm(y, mu, sigma.cand, log=TRUE))
-    ##     prior.sigma.cand <- dunif(sigma.cand, 0, 1000, log=TRUE)
-    ## }
-    ##    mhr <- exp((ll.y.cand+prior.sigma.cand) - (ll.y+prior.sigma))
-
-    ## Symmetric proposal distribution
+    ## Asymmetric proposal distribution - most commonly due to bounds on parameter values
+    # Example: sigma cannot be negative 
+    # Use the log normal distribution to ensure positive sigma values 
     sigma.cand <- rlnorm(1, log(sigma), tune[3])
     ll.y <- sum(dnorm(y, mu, sigma, log=TRUE))
+    # Prior probability distribution of sigma
     prior.sigma <- dunif(sigma, 0, 1000, log=TRUE)
+    # Proportion distribution of sigma
     prop.sigma <- dlnorm(sigma, log(sigma.cand), tune[3], log=TRUE)
 
     ll.y.cand <- sum(dnorm(y, mu, sigma.cand, log=TRUE))
+    # Prior probability distribution of candidate sigma
     prior.sigma.cand <- dunif(sigma.cand, 0, 1000, log=TRUE)
+    # Proportion distribution of candidate 
     prop.sigma.cand <- dlnorm(sigma.cand, log(sigma), tune[3], log=TRUE)
 
+    # P(data | candidate sigma) * P(candidate sigma) * P(sigma | candidate sigma) / P(data | sigma) * P(sigma) * P(candidate sigma | sigma)  
     mhr <- exp((ll.y.cand+prior.sigma.cand+prop.sigma) -
                (ll.y+prior.sigma+prop.sigma.cand))
 
@@ -143,8 +186,8 @@ for(iter in 1:niter) {
 
 return(samples)
 
-
 }
+
 
 
 
@@ -166,18 +209,22 @@ hist(mc1[,"beta0"])
 
 library(coda)
 
-
+# Create a Markov chain Monte Carlo object for the
 mc1.1 <- mcmc(mc1)
 
+# Extract a subset of data between start and end (exclude 1st 100 iterations)
 mc1.1t <- window(mc1.1, start=101, thin=1)
 
 summary(mc1.1t)
 
-rejectionRate(mc1.1t) ## Should be somewhere between 20 and 40 percent
+# Evaluate rejection rate for MH ratio
+# Should be somewhere between 20 and 40 percent
+# This is where tuning comes in 
+rejectionRate(mc1.1t) 
 
 cbind(Est=mles, SE=SEs)
 
-
+# Allows for visualization of gibbs sampler 
 plot(mc1.1t)
 
 
@@ -189,13 +236,15 @@ plot(mc1.1t)
 
 
 library(rjags)
-
+# Create a list of data for jags
 jd <- list(y=y, x=x, n=n)
 str(jd)
 
+# Define your variable names
 jp <- c("beta0", "beta1", "sigma")
 jp
 
+# Set initial values 
 ji <- function() {
     list(beta0=rnorm(1), beta1=rnorm(1), sigmaSq=runif(1))
 }
@@ -204,6 +253,7 @@ ji()
 
 
 ## Compile the model and adapt
+# requires creation of jag file in text editor
 jm <- jags.model("lm-JAGS.jag", data=jd, inits=ji, n.chains=3,
                  n.adapt=1000)
 
