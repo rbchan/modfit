f9f883d69702efde3c9eefbe0d5c9d355a82f061
Richard Chandler <richard.chandlers@gmail.com>
2017-10-27 09:53:10 -0400

    PDF compiles nicely from .Rnw -- now working on .html version

---
+++ keys/lm/lm-build.R
@@ -0,0 +1,48 @@
+## Create .tex file
+library(knitr)
+knit("lm-key.Rnw")
+
+pandoc("lm-key.Rnw")
+
+## Compile and open PDF
+tools::texi2pdf("lm-key.tex")
+system("open lm-key.pdf")
+
+
+## It seems we can't easily convert our .Rnw or .tex to .html
+## but here's a try with pandoc
+## Convert .tex file to .html and .md
+system("pandoc -s lm-key.tex --mathjax -o lm-key.html")
+
+
+## A tedious workaround is to create a .md and then manually insert
+## the R code where it's missing
+## We could just start with .Rmd, but markdown isn't as flexible as Latex
+## Uncomment the next region if you want to do this
+## system("pandoc -s lm-key.tex --mathjax -t markdown -o lm-key.Rmd")
+
+## Once the .Rmd file is ready, you can build html like this
+render("lm-key.Rmd", "html_document")
+
+
+
+
+
+
+
+
+
+## BAD IDEAS BELOW
+
+
+## Convert the .R file to .html
+library(rmarkdown)
+knit("lm-key.Rnw", tangle=TRUE)
+render("lm-key.R", "all")
+render("lm-key.R", "md_document")
+
+render("lm-key.Rnw", "html_document")
+
+
+## Try with htlatex (fails?)
+##system("htlatex lm-key.tex 'xhtml, mathml, charset=utf-8' ' -cunihtf -utf8'")
