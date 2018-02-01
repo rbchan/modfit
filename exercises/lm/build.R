644ba73a459ee5789ef630eef0ec10fbd4afcb0a
Richard Chandler <richard.chandlers@gmail.com>
2018-02-01 14:26:12 -0500

    Improving the lm example

--- exercises/lm/build.R
+++ exercises/lm/build.R
@@ -2,5 +2,8 @@ library(rmarkdown)
 
 
 render("lm.Rmd", "html_document")
-##render("lm.Rmd", "md_document")
+system("open lm.html")
+
+    ##render("lm.Rmd", "md_document")
 render("lm.Rmd", md_document(variant = "markdown", pandoc_args="--mathjax"))
+
