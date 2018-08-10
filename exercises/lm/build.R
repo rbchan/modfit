4d052c0c19755044568e76236ccff154d64a9281
Richard Chandler <richard.chandlers@gmail.com>
2018-08-10 11:39:54 -0400

    Hack to paste mathjax code into html doc created by render

--- exercises/lm/build.R
+++ exercises/lm/build.R
@@ -8,10 +8,15 @@ library(rmarkdown)
 
 
 
-render("lm.Rmd",
-       output_format=html_fragment(
-           mathjax=TRUE,
-           includes =
-               includes(before_body="mathjax.html"),
-           self_contained=TRUE),
-       output_dir="../../_includes")
+out.file <- render("lm.Rmd",
+                   output_format=html_fragment(
+                       mathjax=TRUE,
+    ##           includes =
+    ##               includes(before_body="mathjax.html"),
+                       self_contained=TRUE),
+                   output_dir="../../_includes")
+
+lm.html.in <- readLines(out.file)
+mathjax.in <- readLines("mathjax.html")
+lm.html.out <- c(mathjax.in, lm.html.in)
+writeLines(lm.html.out, out.file)
