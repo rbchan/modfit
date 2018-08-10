53bbf7ed8237a839aa761f76c85db88c55658652
Richard Chandler <richard.chandlers@gmail.com>
2018-08-10 09:00:11 -0400

    Put rmarkdown html in _includes

--- exercises/lm/build.R
+++ exercises/lm/build.R
@@ -2,4 +2,5 @@ library(rmarkdown)
 
 # render("lm.Rmd", output_format=html_document(self_contained=FALSE))
 
-render("lm.Rmd", output_format=html_document(self_contained=TRUE))
+render("lm.Rmd", output_format=html_document(self_contained=FALSE),
+       output_dir="../../_includes")
