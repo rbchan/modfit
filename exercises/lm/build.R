3a7e0b2a24c055db874f5cf9944d22c8698534fa
Richard Chandler <richard.chandlers@gmail.com>
2018-08-10 09:15:48 -0400

    Trying html_fragment instead of html_document

--- exercises/lm/build.R
+++ exercises/lm/build.R
@@ -1,7 +1,14 @@
 library(rmarkdown)
 
-render("lm.Rmd", output_format=html_document(self_contained=TRUE),
-       output_dir="../../_includes")
+## render("lm.Rmd", output_format=html_document(self_contained=TRUE),
+##        output_dir="../../_includes")
 
 ## render("lm.Rmd", output_format=html_document(self_contained=FALSE),
 ##        output_dir="../../_includes")
+
+
+
+render("lm.Rmd",
+       output_format="html_fragment",
+       ##html_fragment(self_contained=TRUE),
+       output_dir="../../_includes")
