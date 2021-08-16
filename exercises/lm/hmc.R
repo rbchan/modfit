f718ea1e08fecc8dc7ab1edc1668b7b8cc9f27ac
Richard Chandler <richard.chandlers@gmail.com>
2021-08-16 13:54:01 -0400

    hmc bug fix

--- exercises/lm/hmc.R
+++ exercises/lm/hmc.R
@@ -47,22 +47,27 @@ hmc.sample.lm <- function(data, n.iter, n.leap, eps) {
 
     L <- 1
     for(i in 1:n.iter) {
+        theta.cand <- theta
         m <- rnorm(n.theta, 0, 1) ## Could use rmvnorm(1, 0, M)
         m.new <- m
         ## Leapfrog
-        ## if(L>n.leap)
-        ##     L <- 2
-        ## for(j in 1:L) {
+        if(i==1)
+            plot(theta[1], theta[2], xlim=c(-5, 3), ylim=c(-3, 4))
         for(j in 1:n.leap) {
-            grad.lp <- as.vector(fg$gr(theta))
-            m.new <- m + eps/2 * grad.lp
-            theta.cand <- theta + eps * Minv %*% m.new
+            theta.old <- theta.cand
+            grad.lp <- as.vector(fg$gr(theta.cand))
+            m.new <- m.new + eps/2 * grad.lp
+            theta.cand <- theta.cand + eps * Minv %*% m.new
             grad.lp.new <- as.vector(fg$gr(theta.cand))
             if(j == n.leap) {
                 m.new <- m.new + eps/2 * grad.lp.new
             }
+            if(i==1) {
+                points(theta.cand[1], theta.cand[2])
+                arrows(theta.old[1], theta.old[2], theta.cand[1], theta.cand[2],
+                       length=0.1, col="red")
+            }
         }
-        ## L <- L+1 ## Cycle through length of leapfrog trajectories
         num <- fg$fn(theta.cand) - 0.5*(m.new %*% Minv %*% m.new)
         den <- fg$fn(theta) - 0.5*(m %*% Minv %*% m)
         if(runif(1) < exp(num - den)) {
@@ -79,7 +84,7 @@ d <- list(y=y, x=x)
 
 ## debugonce(hmc.sample.lm)
 
-samps <- hmc.sample.lm(data=d, n.iter=1000, n.leap=4*1, eps=c(0.2, 0.2, 0.1))
+samps <- hmc.sample.lm(data=d, n.iter=1000, n.leap=4*1, eps=c(0.05, 0.05, 0.03))
 
 plot(samps[,1:2])
 arrows(samps[-100, 1], samps[-100, 2],
